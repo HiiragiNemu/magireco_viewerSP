3b2ee9d2bcd5992be49dae496a5e325b7da1578c
HiiragiNemu <128921071+HiiragiNemu@users.noreply.github.com>
2026-07-29 03:06:36 +0800

    接入语音与 LOG 同步修复

--- js/event.js
+++ js/event.js
@@ -22,6 +22,7 @@
     .then(function () { return window.__MagirecoFullStoryV2Ready; })
     .then(function () { return loadScript('./js/full_story_reliability_patch.js'); })
     .then(function () { return loadScript('./js/story_runtime_v3.js'); })
+    .then(function () { return loadScript('./js/story_runtime_v3_sync_patch.js'); })
     .then(function () { return loadScript('./js/story_runtime_v3_layer_patch.js'); })
     .then(function () {
       if (window.__installMagirecoReliabilityPatch) window.__installMagirecoReliabilityPatch();
